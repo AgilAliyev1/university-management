df58b89f76ba9189d6bd7c3611fbe041bf496bf1
AgilAliyev1 <aqilliyev207@gmail.com>
2025-03-04 10:04:16 +0400

    .

--- model/person/Employee.java
+++ model/person/Employee.java
@@ -2,6 +2,41 @@ package model.person;
 
 import java.time.LocalDate;
 
+// Interface
 public interface Employee {
     LocalDate getHireDate();
+}
+
+// Base class implementing Employee
+class BaseEmployee implements Employee {
+    private String name;
+    private LocalDate hireDate;
+
+    public BaseEmployee(String name, LocalDate hireDate) {
+        this.name = name;
+        this.hireDate = hireDate;
+    }
+
+    @Override
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+// Subclass extending BaseEmployee
+class Manager extends BaseEmployee {
+    private String department;
+
+    public Manager(String name, LocalDate hireDate, String department) {
+        super(name, hireDate);
+        this.department = department;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
 }
